b051cd2a48e91614519927cb1522797f14191501
Paulo Rocha <pv.fonseca.rocha@gmail.com>
2023-04-11 20:45:50 -0300

    Projeto Cadastro Firestore

---
+++ src/pages/index.tsx
@@ -0,0 +1,54 @@
+import Tabela from "../components/Tabela";
+import Layout from "../components/Layout";
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
+import useClientes from "../hooks/useClientes";
+
+export default function Home() {
+
+  const {
+    cliente, 
+    clientes, 
+    novoCliente,
+    salvarCliente,
+    selecionarCliente, 
+    excluirCliente,
+    tabelaVisivel,
+    exibirTabela
+  } = useClientes()
+
+  return (
+    <div className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white 
+    `}>
+      <Layout titulo="Cadastro Simples">
+        {tabelaVisivel ? (
+        <>
+          <div className="flex justify-end">
+            <Botao 
+              cor="green" 
+              className="mb-4" 
+              onClick={novoCliente}>
+              Novo Cliente
+            </Botao>
+          </div>
+    
+          <Tabela clientes={clientes} 
+            clienteSelecionado={selecionarCliente}
+            clienteExcluido={excluirCliente}
+          />
+        </>
+        ) : (
+          <Formulario 
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={exibirTabela}
+          />
+        )}
+      </Layout>
+    </div>
+  )
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    'from-green-400',
+    'from-blue-400',
+    'from-gray-400',
+    'to-gray-700',
+    'to-blue-700',
+    'to-green-700',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
